f98912ace21ed1f817896a372539f3783da166ad
Pavel Bredikhin <bredikhin13@gmail.com>
2014-12-08 01:37:02 +0300

    Взоимодействие с игровым миром
    Более реалистичная гравитация

--- core/src/com/supermario/game/model/Player.java
+++ core/src/com/supermario/game/model/Player.java
@@ -15,7 +15,7 @@ import java.awt.*;
 //методы движение, анимация, взаимодействие с картой, взаимодействие с врагами
 public class Player {
 
-    public int dx,dy; // ускорение по оси Х и Y
+    public int dx, dy; // ускорение по оси Х и Y
     Map map; //карта
     public Vector2 point; //вектор, хранит текущее положение игрока
     public boolean grounded = true; // положение игрока: true - на земле, false - в воздухе
@@ -24,6 +24,7 @@ public class Player {
 
     public Sprite playerSprite;
     Texture texture = new Texture(Gdx.files.internal("assets/player.png"));
+
     public Player(Map map, final float x, float y) {
         dx = 0;
         dy = 0;
@@ -47,34 +48,40 @@ public class Player {
 
     public void up(int f) {
     }
-    void Collision(char dir){
-        for(int i = (int)(18-(point.y+54)/30);i<=(int)(18-point.y/30);i++)
-            for(int j = (int)point.x/30;j<=(int)(point.x+54)/30;j++){
-                if (map.linesArray[i].charAt(j)=='B'){
-                    if (dx>0 && dir=='x') point.x=j*30-54-1;
-                    if (dx<0 && dir=='x') point.x = (j+1)*30+1;
-                    if (dy>0 && dir=='y') {
-                        point.y = (i)*30-54-1;
+
+    void Collision(char dir) {
+        for (int i = (int) (18 - (point.y + 54) / 30); i <= (int) (18 - point.y / 30); i++)
+            for (int j = (int) point.x / 30; j <= (int) (point.x + 54) / 30; j++) {
+                if (map.linesArray[i].charAt(j) == 'B') {
+                    if (dx > 0 && dir == 'x') point.x = j * 30 - 54 - 1;
+                    if (dx < 0 && dir == 'x') point.x = (j + 1) * 30 + 1;
+                    if (dy > 0 && dir == 'y') {
+                        point.y = (18-i-1) * 30 - 54 - 1;
                         dy = 0;
                     }
-                    if (dy<0 && dir=='y') {
-                        dy = 0;
+                    if (dy < 0 && dir == 'y') {
                         grounded = true;
-                        point.y = (18-i)*30+1;
+                        dy = 0;
+
+                        point.y = (18 - i) * 30 + 1;
                     }
                 }
 
             }
 
     }
+
     public void playerMove() {
         playerSprite.setX(point.x += dx * Gdx.graphics.getDeltaTime());
         Collision('x');
+        if (grounded  && (map.linesArray[(int) ((18 - point.y / 30) + 1)]).charAt((int) point.x / 30) == ' ') {
+            dy = -400;
+            grounded = false;
+        }
         dx = 0;
-        if (!grounded){
-
+        if (!grounded) {
             playerSprite.setY(point.y += dy * Gdx.graphics.getDeltaTime());
-            dy-=20;
+            dy -= 20;
             Collision('y');
         }
 //        if (playerSprite.getY() <= 20) {
